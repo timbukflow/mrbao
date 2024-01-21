452324aab18a5fbbe06a70a78bf4b1c383c9f7e1
Ivo Schwizer <ivoschwizer@gmail.com>
2024-01-21 19:59:01 +0100

    first

---
+++ footer.php
@@ -0,0 +1,56 @@
+<footer>
+    <div class="footercont fadein">
+        <div class="footerfirst">
+            <a class="totop" href="index">BILTEC AG <br> Ihre CNC-Profis</a>
+            <a href="download/biltec-ag-sqs-zertifikat.pdf" target="_blank"> Wir sind SQS-zertifiziert.</a>
+        </div>
+        <div class="footernav">
+            <ul>
+                <li class="passiv">Navigation</li>
+                <li class="group">
+                    <ul>
+                        <li><a href="produkte">Produkte</a></li>
+                        <li><a href="rotative-loesungen">Rotative Lösungen</a></li>
+                        <li><a href="kubische-loesungen">Kubische Lösungen</a></li>
+                        <li><a href="baugruppenmontage">Baugruppenmontage</a></li>
+                    </ul>
+                </li>
+                <li class="group">
+                    <ul>
+                        <li><a href="unternehmen">Unternehmen</a></li>
+                        <li><a href="unternehmen#qualitaet-first">Qualität first</a></li>
+                        <li><a href="unternehmen#maschinenpark">Maschinenpark</a></li>
+                        <li><a href="unternehmen#ansprechpersonen">Ansprechpersonen</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul>
+                <li class="passiv">Kontakt</li>
+                <li class="group">
+                    <ul>
+                        <li>
+                            <a href="https://maps.app.goo.gl/ZHc5n5xiEurBCFQa9" target="_blank">Biltec AG<br>
+                            Unterlettenstrasse 10<br>
+                            CH-9443 Widnau</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="group">
+                    <ul>
+                        <li><a href="mailto:info@biltec.ch" target="_blank">Mail info(at)biltec.ch</a></li>
+                        <li><a href="tel:+41717201787">Telefon +41 71 720 17 87</a></li>
+                    </ul>
+                </li>
+                <li class="group">
+                    <ul>
+                        <li><a href="impressum">Impressum</a></li>
+                        <li><a href="datenschutz">Datenschutz</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="footercopy">
+        <p>© Copyright 2024 BILTEC AG | <span>Realisierung <a href="https://schwizerdesign.ch/" target="_blank">schwizerdesign.ch</a></span></p>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,247 @@
+<!-- 
+
+Made by Schwizer Design GmbH
+Say hello @ schwizerdesign.ch
+Version 1.0
+
+-->
+
+<!DOCTYPE html>
+<html lang="de">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# place: http://ogp.me/ns/place#">
+
+    <meta charset="UTF-8">
+    <title>Biltec AG | Ihre CNC-Profis</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="BILTEC AG - Ihr verlässlicher Partner für CNC-Fertigung, Konstruktion, Messtechnik und Baugruppenmontage. Spezialisiert auf Druckzubehör und kreative Lösungen mit modernster Technologie, Wirtschaftlichkeit und Nachhaltigkeit.">
+    <meta name="keywords" content="CNC-Fertigung, Konstruktion, Messtechnik, Baugruppenmontage, Druckzubehör, kreative Lösungen, moderne Technologie, Wirtschaftlichkeit, Nachhaltigkeit, Verlässlichkeit, Flexibilität, Biltec">
+    <meta name="author" content="Biltec AG">
+    <link rel="canonical" href="https://biltec.ch" />
+
+    <link rel="alternate" hreflang="de" href="https://biltec.ch/" />
+    <link rel="alternate" hreflang="en" href="https://biltec.ch/en" />
+
+    <meta name="robots" content="index, follow" /> 
+    <meta http-equiv="cache-control" content="public, max-age=3600" />
+    <meta http-equiv="pragma" content="cache" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4690ff">
+    <meta name="msapplication-TileColor" content="#002155">
+    <meta name="theme-color" content="#ffffff">
+
+    <meta property="og:title" content="Biltec AG | Ihre CNC-Profis">
+    <meta property="og:description" content="BILTEC AG - Ihr verlässlicher Partner für CNC-Fertigung, Konstruktion, Messtechnik und Baugruppenmontage. Spezialisiert auf Druckzubehör und kreative Lösungen mit modernster Technologie, Wirtschaftlichkeit und Nachhaltigkeit.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://biltec.ch">
+    <meta property="og:image" content="https://biltec.ch/img/og-image.jpg">
+    <meta property="og:locale" content="de_CH">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Biltec AG | Ihre CNC-Profis">
+    <meta name="twitter:description" content="BILTEC AG - Ihr verlässlicher Partner für CNC-Fertigung, Konstruktion, Messtechnik und Baugruppenmontage. Spezialisiert auf Druckzubehör und kreative Lösungen mit modernster Technologie, Wirtschaftlichkeit und Nachhaltigkeit.">
+    <meta name="twitter:image" content="https://biltec.ch/img/twitter-image.jpg">
+
+    <meta name="format-detection" content="telephone=yes">
+    <meta property="business:contact_data:street_address" content="Unterlettenstrasse 10">
+    <meta property="business:contact_data:locality" content="Widnau"> 
+    <meta property="business:contact_data:region" content="9443">
+    <meta property="business:contact_data:postal_code" content="9443">
+    <meta property="business:contact_data:country_name" content="Switzerland">
+    
+    <link rel="stylesheet" href="main.css" />
+
+    <script type="application/ld+json">
+    {
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "name": "Biltec AG",
+        "description": "BILTEC AG - Ihr Partner für CNC-Fertigung, Konstruktion, Messtechnik und Baugruppenmontage.",
+        "url": "https://biltec.ch",
+        "contactPoint": {
+            "@type": "ContactPoint",
+            "telephone": "+41717201787",
+            "contactType": "Kundenservice"
+        },
+        "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "Unterlettenstrasse 10",
+            "addressLocality": "Widnau",
+            "addressRegion": "SG",
+            "postalCode": "9443",
+            "addressCountry": "CH"
+        },
+        "sameAs": [
+            "https://www.linkedin.com/in/dietmar-thoeny-63bb3482/",
+            "https://www.linkedin.com/in/miodrag-kostic-b417332a3/"
+        ]
+    }
+    </script>
+
+</head>
+
+<body>
+    <header class="main fadein">
+        <h1 class="big">Ihre CNC-<br>Profis</h1>
+        <h2 class="headline">
+            Die <strong>BILTEC AG</strong> ist Ihr vielseitiger Partner, wenn es um CNC-Fertigung, Konstruktion, Messtechnik und Montage von Baugruppen geht. Wir sind nicht nur Spezialisten für Druckzubehör, sondern übernehmen auch gerne Aufträge, bei denen kreative Ideen und technisches Know-how gefragt sind.
+        </h2>
+
+        <article class="teaser">
+            <div id="rotativ" class="animatC fadein">
+                <img id="rotativImg" src="img/rotativ0.webp" alt="Plattenzylinder für die Druckindustrie, gefertigt durch Biltec CNC-Fertigung. Abmessungen: Durchmesser 200mm x Länge 630mm, aus Wälzlagerstahl. Teil der rotativen Lösungen.">
+                <p>Plattenzylinder - Druckindustrie<br>Abmasse: Ø200x630 <br> Material: Wälzlagerstahl</p>
+            </div>
+            <h2 class="big fadein">Rotativ</h2>
+            <h3 class="headline fadein">
+                Mit Hilfe unseres Dreh- und Fräscenters sowie unseren CNC-Rundschleifmaschinen fertigen wir Bauteile von hoher Komplexität – und zwar in einem wirtschaftlich attraktiven Rahmen.
+            </h3>
+            <div class="teaserBtn fadein">
+                <a href="rotative-loesungen">
+                    <button>Rotative Lösungen</button>
+                </a>
+                <a href="rotative-loesungen#druckzubehoer">
+                    <button class="white">Druckzubehör</button>
+                </a>
+            </div>
+        </article>
+
+        <article class="teaser">
+            <div id="kubisch" class="animatC fadein">
+                <img id="kubischImg" src="img/kubisch0.webp" alt="Dualadapter für die Optikindustrie, hergestellt durch Biltecs kubische CNC-Fertigung. Gefertigt aus hochfestem Aluminium. Spezialisiertes Bauteil für präzise Anwendungen.">
+                <p>Dualadapter - Optikindustrie <br>Abmasse: 25x150x350 <br> Material: Hochfestes Aluminium</p>
+            </div>
+            <h2 class="big fadein">Kubisch</h2>
+            <h3 class="headline fadein">
+                Dank unseren 4- und 5-Achs-Fräszentren fertigen wir sämtliche gewünschten Bauteile in der zugesicherten Qualität – eine für Sie auch wirtschaftlich höchst attraktive Lösung.
+            </h3>
+            <div class="teaserBtn fadein">
+                <a href="kubische-loesungen">
+                    <button>Kubische Lösungen</button>
+                </a>
+            </div>
+        </article>
+    </header>
+
+    <div class="mainfull">
+        <div class="numbers fadein">
+            <div><h3><span class="counter" data-target="100">0</span>Tonnen Stahl</h3></div>
+            <div><h3><span class="counter" data-target="300">0</span>Tonnen Aluminium</h3></div>
+            <div><h3><span class="counter" data-target="40">0</span>Tonnen Edelstahl</h3></div>
+        </div>
+        <div class="imgC fadein">
+            <picture>
+                <source type="image/webp" srcset="img/biltec-cnc-profis-kubisch-werkstoffe-stahl-aluminium-edelstahl.webp" >
+                <source type="image/jpg" srcset="img/biltec-cnc-profis-kubisch-werkstoffe-stahl-aluminium-edelstahl.jpg" >
+                <img src="img/biltec-cnc-profis-kubisch-werkstoffe-stahl-aluminium-edelstahl.jpg" alt="Verschiedene Werkstoffe für kubische CNC-Fertigung bei Biltec: Stahl, Aluminium und Edelstahl. Die Materialien symbolisieren die Vielseitigkeit und Qualität der Bearbeitungsmöglichkeiten." loading="lazy">
+            </picture>
+        </div>
+        <div class="description">
+            <p>
+                Dies ist die durchschnittliche Menge an Metallen, die wir in unseren Werkhallen pro Jahr verarbeiten.
+            </p>
+        </div>
+    </div>
+
+    <main class="mainwhite">
+        <div class="subtitle fadein">
+            <h2>Was Sie alles von uns haben</h2>
+        </div>
+        <div class="gridC">
+            <div class="iconC fadein">
+                <img src="img/biltec-icon-spitzentechnologie.svg" alt="biltec icon spitzentechnologie">
+                <h3>Spitzentechnologie</h3>
+                <p>
+                    Die <strong>BILTEC AG</strong> steht nie still. So integrieren wir ständig die modernsten Technologien in unseren Fertigungspark und entwickeln uns laufend weiter, damit unsere Produkte jederzeit dem aktuellsten Stand entsprechen.
+                </p>
+            </div>
+            <div class="iconC fadein">
+                <img src="img/biltec-icon-wirtschaftlichkeit.svg" alt="biltec icon wirtschaftlichkeit">
+                <h3>Wirtschaftlichkeit</h3>
+                <p>
+                    Die <strong>BILTEC AG</strong> steht für eine effiziente Produktion – dank hoher Verfügbarkeit, durchdachten Prozessen, einem modernen Maschinenpark sowie Energieeffizienz.
+                </p>
+            </div>
+            <div class="iconC fadein">
+                <img src="img/biltec-icon-partnerschaft.svg" alt="biltec icon partnerschaft">
+                <h3>Partnerschaft</h3>
+                <p>
+                    Die <strong>BILTEC AG</strong> steht Kunden als starker Partner zur Seite, vom ersten Kontakt bis zur Auslieferung und darüber hinaus. Unsere vielen langjährigen Beziehungen sind der Beweis.
+                </p>
+            </div>
+        </div>
+        <div class="gridC">
+            <div class="iconC fadein">
+                <img src="img/biltec-icon-verantwortung.svg" alt="biltec icon verantwortung">
+                <h3>Verantwortung</h3>
+                <p>
+                    Die <strong>BILTEC AG</strong> ist sich der Verantwortung gegenüber ihren Stakeholdern, der Gesellschaft sowie der Umwelt bewusst. Nachhaltigkeit haben wir deshalb fest in unserem Unternehmen verankert und unser tägliches Handeln darauf abgestimmt.
+                </p>
+            </div>
+            <div class="iconC fadein">
+                <img src="img/biltec-icon-zuverlaessigkeit.svg" alt="biltec icon zuverlässigkeit">
+                <h3>Zuverlässigkeit</h3>
+                <p>
+                    Auf die <strong>BILTEC AG</strong> ist Verlass. Als erfahrener und kompetenter Partner liefern wir in höchster Qualität – termingerecht und zu den vereinbarten Konditionen.
+                </p>
+            </div>
+            <div class="iconC fadein">
+                <img src="img/biltec-icon-flexibilitaet.svg" alt="biltec icon flexibilität">
+                <h3>Flexibilität</h3>
+                <p>
+                    Die <strong>BILTEC AG</strong> steht für eine klare Haltung: Wir loten die Grenzen des Machbaren immer wieder neu aus. Das heisst, unsere Spezialisten setzen immer alles daran, die Wünsche der Kunden zu erfüllen.
+                </p>
+            </div>
+        </div>
+    </main>
+    
+    <div class="mainsubnav">
+        <div class="subnav fadein">
+            <div class="subnavimg">
+                <picture>
+                    <source type="image/webp" srcset="img/biltec-cnc-profis-teaser-unternehmen.webp" >
+                    <source type="image/jpg" srcset="img/biltec-cnc-profis-teaser-unternehmen.jpg" >
+                    <img src="img/biltec-cnc-profis-teaser-unternehmen.jpg" alt="CNC-Fertigungsanlage mit einer Auswahl verschiedener Bohrköpfe für rotative und kubische Bearbeitungsverfahren." loading="lazy">
+                </picture>
+            </div>
+            <div class="subnavcontent">
+                <h2 >Biltec AG - Ihr CNC-Profis</h2>
+                <p>
+                    Seit über zwanzig Jahren sind wir am Markt. Heute sind wir weit über die Schweiz hinaus bekannt als renommierter Anbieter von mechanischen Fertigungsteilen höchster Güteklasse.
+                </p>
+            </div>
+            <a href="unternehmen">
+                <button>Unternehmen</button>
+            </a>
+        </div>
+        <div class="subnav fadein">
+            <div class="subnavimg">
+                <picture>
+                    <source type="image/webp" srcset="img/biltec-cnc-profis-teaser-prudukte.webp" >
+                    <source type="image/jpg" srcset="img/biltec-cnc-profis-teaser-prudukte.jpg" >
+                    <img src="img/biltec-cnc-profis-teaser-prudukte.jpg" alt="Innenansicht einer CNC-Maschine bei Biltec, spezialisiert auf rotative Fertigungen. Detailaufnahme zeigt Präzisionswerkzeuge und -komponenten in Aktion, verdeutlicht die technologische Expertise und Fertigungskompetenz im Bereich der rotativen CNC-Bearbeitung." loading="lazy">
+                </picture>
+            </div>
+            <div class="subnavcontent">
+                <h2>Wir haben Ihnen viel zu bieten</h2>
+                <p>
+                    Wir setzen vor Ort auf eine Reihe von Horizontalmaschinen, mit denen wir Präzisionskomponenten aus praktisch allen wichtigen metallischen Werkstoffen herstellen. Von der Konstruktion/CAD/CAM bis zur Baugruppenmontage.
+                </p>
+            </div>
+            <a href="produkte">
+                <button>Produkte</button>
+            </a>
+        </div>
+    </div>
+
+    <?php require_once 'nav.php'; ?>
+    <?php require_once 'footer.php'; ?>
+    <?php require_once 'script.php'; ?>
+    <script src="animation.js"></script>
+    <?php require_once 'googleanalytics.php'; ?>
+</body>
+</html>
